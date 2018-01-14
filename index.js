1fc56c9cf1322e56ee92c5fafa20efcb5643865d
Jozielio Santiago <jozieliosantiago@gmail.com>
2018-01-14 12:56:54 -0300

    modified index

---
+++ index.js
@@ -0,0 +1,47 @@
+const Message = require('./message.model');
+const ImageMessage = require('./image-message.model');
+const fs = require('fs');
+const path = require('path');
+
+const emptyMessage = new Message();
+const emptyPhotoMessage = new ImageMessage();
+
+console.log(emptyMessage);
+console.log(emptyPhotoMessage);
+
+class MessagesService {
+  constructor() {
+    let resolvePromise;
+    let rejectPromise;
+    const filePath = path.join(__dirname, 'messages.json');
+    this.messagePromise = new Promise((resolve, reject) => {
+      resolvePromise = resolve;
+      rejectPromise = reject;
+    });
+    fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
+      if (err) {
+        rejectPromise(err);
+      } else {
+        const dataArray = JSON.parse(data);
+        const dataObj = dataArray.map((item) => {
+          new Message(item.text, item.created);
+        })
+        resolvePromise(dataObj);
+      }
+    })
+  }
+
+  get messages() {
+    return this.messagePromise;
+  };
+};
+
+const messageService = new MessagesService();
+messageService.messages.then((messages) => {
+  for (let x = 0; x < messages.length; x++) {
+    console.log(String(messages[x]));
+  }
+  // console.log(messages);
+}).catch((err) => {
+  console.log(err);
+});
