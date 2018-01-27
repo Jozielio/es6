dfab930ac4d8f04cd0175ee1a1c3bfe2b4ab8576
Jozielio Santiago <jozieliosantiago@gmail.com>
2018-01-27 20:07:01 -0300

    generic in utils

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,6 @@
 import moment from 'moment';
 import Message from './model/message.model';
-import { messageFactory } from './utils';
+import { messageFactory, Store } from './utils';
 
 import './styles/modules/MessageBox.scss';
 import './styles/modules/MessagesArea.scss';
@@ -8,19 +8,21 @@ import './styles/modules/MessagesArea.scss';
 const template: any = require('./messages.html');
 const logo: any = require('./images/especializa_logo.jpg');
 
-console.log('Index started again');
+const store: Store<Message> = new Store<Message>();
 
-(<HTMLButtonElement> document.getElementById('send')).onclick = () => {
-  messageFactory((<HTMLInputElement> document.getElementById('message')).value)
-  .then((m: Message) => {
-    (<HTMLElement> document.getElementById('messages')).innerHTML += template({
+(<HTMLButtonElement>document.getElementById('send')).onclick = () => {
+  messageFactory((<HTMLInputElement>document.getElementById('message')).value)
+    .then((m: Message) => {
+      (<HTMLElement>document.getElementById('messages')).innerHTML += template({
         m,
         relativeTime: moment(m.created).fromNow(),
       });
-  });
+      store.add(m);
+      store.commit();
+    });
 };
 
-(<HTMLImageElement> document.getElementById('logo')).src = logo;
+(<HTMLImageElement>document.getElementById('logo')).src = logo;
 
 if (module && module.hot) {
   module.hot.accept();
--- app/utils.ts
+++ app/utils.ts
@@ -6,19 +6,36 @@ function messageFactory(text: string): Promise<Message> {
   if (giphy.test(text)) {
     const match: string = (text.match(giphy) as string[])[1];
     return fetch('http://api.giphy.com/v1/gifs/search?' +
-          `q=${match}&api_key=dc6zaTOxFJmzC`, {
-      method: 'GET',
-      mode: 'cors',
-    })
-    .then((response: Response) => response.json())
-    .then((result: any) => {
-      const replaced = text.replace(giphy,
-        `<img src='${result.data[0].images.fixed_height_small.url}'>`);
-      return new ImageMessage(replaced, undefined, result.data[0].embed_url);
-    })
-    .catch(() => new Message(text)) as Promise<Message>;
+      `q=${match}&api_key=dc6zaTOxFJmzC`, {
+        method: 'GET',
+        mode: 'cors',
+      })
+      .then((response: Response) => response.json())
+      .then((result: any) => {
+        const replaced = text.replace(giphy,
+          `<img src='${result.data[0].images.fixed_height_small.url}'>`);
+        return new ImageMessage(replaced, undefined, result.data[0].embed_url);
+      })
+      .catch(() => new Message(text)) as Promise<Message>;
   }
   return Promise.resolve(new Message(text));
 }
 
-export { messageFactory };
+class Store<T> {
+  private store: Set<T>;
+  constructor() {
+    const sStore: (string | null) = localStorage.getItem('store');
+    this.store = sStore ? JSON.parse(sStore) : new Set<T>();
+  }
+  public add(item: T): void {
+    this.store.add(item);
+  }
+
+  public commit(): void {
+    localStorage.setItem('store', JSON.stringify(Array.from(this.store)));
+  }
+  public list(): T[] {
+    return Array.from(this.store);
+  }
+}
+export { messageFactory, Store };
